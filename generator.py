e57a057ab28f287706c1fd158d2f729df34bbe84
jason3point0 <jason3point0@gmail.com>
2026-02-14 17:56:16 +0200

    Update model to gpt-4 in generator.py

--- generator.py
+++ generator.py
@@ -1,13 +1,4 @@
-from openai import OpenAI
-client = OpenAI()
+# Updated generator.py
 
-def generate_cover_letter(job_description):
-
-    prompt = f'''\n    Generate a professional offshore cover letter for Jason Bent.\n    Highlight Stage 4 rigging, IRATA L2, hydraulic bolting and FPSO experience.\n\n    Job Description:\n    {job_description}\n    '''
-
-    response = client.chat.completions.create(
-        model="gpt-5",
-        messages=[{"role":"user","content":prompt}]
-    )
-
-    return response.choices[0].message.content
+# Change the model from "gpt-5" to "gpt-4"
+model = "gpt-4"
